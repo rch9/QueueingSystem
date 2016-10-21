668df5021dbde2d9746b91c37869ebda598723f5
rch_9 <enlofpoliteh121@gmail.com>
2016-10-21 23:48:36 +0300

    added working qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "gtest/gtest.h"
+//#include <QtCore>
+#include <QApplication>
+#include <QtWidgets>
+//#include <QLabel>
+//#include <Qt  Gui>
 
 int main(int argc, char *argv[]) {
 //    ::testing::InitGoogleTest(&argc, argv);
 
 
+    QApplication app(argc, argv);
+    QDialog *dialog = new QDialog;
+    QLabel *label = new QLabel(dialog);
+    label->setText("<font color=red>Hello, World!</font>");
+    dialog->show();
+    return app.exec();
 
 
-    return 0;//RUN_ALL_TESTS();
+
+//    return RUN_ALL_TESTS();
 }
