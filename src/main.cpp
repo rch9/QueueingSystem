789bec92c46b738801263e7decb5c0b31626ed9f
rch_9 <enlofpoliteh121@gmail.com>
2016-10-21 20:33:54 +0300

    fix finding libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "iostream"
 
-int main(int argc, char const *argv[]) {
-    std::cout << "hello world" << std::endl;
-    return 0;
+// #include "external/gtest/include/gtest/gtest.h"
+#include "gtest/gtest.h"
+
+//int main(int argc, char const *argv[]) {
+//    std::cout << "hello world" << std::endl;
+//    return 0;
+//}
+
+TEST(FactorialTest, Negative) {
+  EXPECT_EQ(1, 1);
 }
