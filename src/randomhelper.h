24c2e1b6a5f1de5723b2183c3495b9c808087aaa
rch_9 <enlofpoliteh121@gmail.com>
2016-11-18 22:50:49 +0300

    little fixe

--- src/randomhelper.h
+++ src/randomhelper.h
@@ -7,15 +7,12 @@
 
 class RandomHelper
 {
-public:
-    RandomHelper();
-
+public:   
     static float rand_exponential(const float &lambda);
     static float rand_uniform(const int &first, const int &second);
 
 private:
-
-
+    RandomHelper();
 };
 
 #endif // RANDOMHELPER_H
