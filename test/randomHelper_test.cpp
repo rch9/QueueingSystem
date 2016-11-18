ea449c6980cb2060d0a7805b7619fd0f8d2a1e86
rch_9 <enlofpoliteh121@gmail.com>
2016-11-18 22:44:39 +0300

    add randHelper test

---
+++ test/randomHelper_test.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "src/randomhelper.h"
+#include "iostream"
+
+/**
+ * @brief TEST
+ * Затестить на нулях
+ */
+
+
+TEST(RandomHelperTest, randUniform) {
+
+
+}
+
+//TEST(BufferTest1, Negative) {
+////    EXPECT_EQ(1, 1);
+//}
+
+//TEST(BufferTest2, Negative) {
+////    EXPECT_EQ(1, 1);
+//}
+
+
+
