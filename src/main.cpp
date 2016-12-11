82d62795acf86b64362c5c312a24d402eb5018cc
rch_9 <enlofpoliteh121@gmail.com>
2016-12-11 23:14:49 +0300

    added 2 tabs

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,41 @@
 #include "gtest/gtest.h"
 #include <QApplication>
 #include <QtWidgets>
+#include "QStandardItemModel"
+#include "QStandardItem"
 
 int main(int argc, char *argv[]) {
-    ::testing::InitGoogleTest(&argc, argv);
-
+    //    ::testing::InitGoogleTest(&argc, argv);
 
     QApplication app(argc, argv);
-    QDialog *dialog = new QDialog;
-    QLabel *label = new QLabel(dialog);
-    label->setText("<font color=red>Hello, World!</font>");
-    dialog->show();
-    app.exec();
+
+    QTabWidget tab;
+
+    QPalette Pal;
+
+    // устанавливаем цвет фона
+    Pal.setColor(QPalette::Background, Qt::black);
 
 
+    QWidget window1, window2;
 
-    return RUN_ALL_TESTS();
+    tab.resize(600, 600);
+    window1.resize(600, 400);
+    window2.resize(600, 400);
+
+    window1.setAutoFillBackground(true);
+    window1.setPalette(Pal);
+    window1.setFixedHeight(400);
+    window2.setAutoFillBackground(true);
+    window2.setPalette(Pal);
+    window2.setFixedHeight(400);
+
+
+    tab.addTab(&window1, "first");
+    tab.addTab(&window2, "second");
+
+    tab.show();
+
+    return app.exec();
+    //    return RUN_ALL_TESTS();
 }
