c82c791e7e45bd059b037dcbb8c455bd429c1d9f
rch_9 <enlofpoliteh121@gmail.com>
2016-12-12 01:24:08 +0300

    added pointers

--- src/ui/mainwidget.cpp
+++ src/ui/mainwidget.cpp
@@ -6,7 +6,10 @@
 
 
 MainWidget::MainWidget():
-    QWidget() {
+    QWidget(),
+    table(new TabTableWidget()),
+    smo(new TabSMOWidget()),
+    tabs(new QTabWidget()) {
 
     QPalette Pal;
     Pal.setColor(QPalette::Background, Qt::black);
@@ -15,13 +18,22 @@ MainWidget::MainWidget():
     initMainWidget();
 }
 
+// НУ ТУТ СРЕМНО, ДА
+MainWidget::~MainWidget() {
+    delete table;
+    delete smo;
+    delete tabs;
+}
+
 void MainWidget::initMainWidget() {
-    tabs.setParent(this);
-    tabs.setFixedSize(600, 400);
+    tabs->setParent(this);
+    tabs->setFixedSize(600, 400);
+
+
 
 //    setFixedSize(600, 600);
     resize(600, 600);
 
-    tabs.addTab(&table, "first");
-    tabs.addTab(&smo, "second");
+    tabs->addTab(table, "first");
+    tabs->addTab(smo, "second");
 }
--- src/ui/mainwidget.h
+++ src/ui/mainwidget.h
@@ -2,18 +2,20 @@
 #define MAINWIDGET_H
 #include <QTabWidget>
 #include <QWidget>
+#include <QPushButton>
 #include "tabtablewidget.h"
 #include "tabsmowidget.h"
 
 class MainWidget : public QWidget {
 public:
     MainWidget();
+    ~MainWidget();
     void initMainWidget();
 
 private:
-    TabTableWidget table;
-    TabSMOWidget smo;
-    QTabWidget tabs;
+    TabTableWidget* table;
+    TabSMOWidget* smo;
+    QTabWidget* tabs;
 
 };
 
