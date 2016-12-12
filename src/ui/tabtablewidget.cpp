2e5be76a840778139b87d4468ea7dcc534a29b75
rch_9 <enlofpoliteh121@gmail.com>
2016-12-12 17:02:39 +0300

    added tablewidget

--- src/ui/tabtablewidget.cpp
+++ src/ui/tabtablewidget.cpp
@@ -3,13 +3,33 @@
 #include <iostream>
 
 TabTableWidget::TabTableWidget():
-    QWidget() {
+    QWidget(),
+    _tableWidget(new QTableWidget()) {
 
     QPalette Pal;
     Pal.setColor(QPalette::Background, Qt::white);
     setPalette(Pal);
 
     resize(600, 400);
-    setAutoFillBackground(true);
+//    setAutoFillBackground(true);
     setFixedSize(600, 400);
+
+    initTabTableWidget();
+}
+
+void TabTableWidget::initTabTableWidget() {
+    _tableWidget->setParent(this);
+    QStringList list;
+
+        _tableWidget->setColumnCount(5);
+        _tableWidget->setRowCount(6);
+
+    list.push_back("row1");
+    list.push_back("row2");
+    list.push_back("row3");
+    list.push_back("row4");
+    list.push_back("row5");
+    _tableWidget->setHorizontalHeaderLabels(list);
+
+    _tableWidget->setFixedSize(600, 400);
 }
--- src/ui/tabtablewidget.h
+++ src/ui/tabtablewidget.h
@@ -1,11 +1,17 @@
 #ifndef TABTABLEWIDGET_H
 #define TABTABLEWIDGET_H
 #include <QWidget>
+#include <QTableWidget>
 
-
+//FIXME
 class TabTableWidget : public QWidget {
 public:
     TabTableWidget();
+
+private:
+    void initTabTableWidget();
+private:
+    QTableWidget *_tableWidget;
 };
 
 #endif // TABTABLEWIDGET_H
