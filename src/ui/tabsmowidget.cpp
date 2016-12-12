0538bdb614df96bec3e6b7307213f687e9732241
rch_9 <enlofpoliteh121@gmail.com>
2016-12-12 16:37:10 +0300

    added smowidget with labels

--- src/ui/tabsmowidget.cpp
+++ src/ui/tabsmowidget.cpp
@@ -2,8 +2,7 @@
 //#include <QApplication>
 
 TabSMOWidget::TabSMOWidget():
-    QWidget(),
-    label(new QLabel()) {
+    QWidget() {
 
     QPalette Pal;
     Pal.setColor(QPalette::Background, Qt::white);
@@ -12,8 +11,42 @@ TabSMOWidget::TabSMOWidget():
     resize(600, 400);
     setAutoFillBackground(true);
     setFixedSize(600, 400);
+
+    initSMO();
+
 }
 
 void TabSMOWidget::initSMO() {
+    for(int i = 0; i < 12; ++i) {
+        _labels.push_back(new QLabel());
+    }
 
+    auto myfunc = [&](QLabel* label, int counter, int offsetX){
+        label->setParent(this);
+        label->setText("MY TEXT");
+        label->move(offsetX, 20 + 20*counter);
+    };
+
+    for(int i = 0; i < 4; ++i) {
+        myfunc(_labels.at(i + 0), i, 100);
+        myfunc(_labels.at(i + 4), i, 200);
+        myfunc(_labels.at(i + 8), i, 300);
+    }
 }
+
+void TabSMOWidget::setSourceValue(const int &sourceNum, const float &value) {
+    _labels.at(sourceNum)->setText(convertFloatToString(value));
+}
+
+void TabSMOWidget::setBufferValue(const int &bufferSlotNum, const float &value) {
+    _labels.at(bufferSlotNum + 4)->setText(convertFloatToString(value));
+}
+
+void TabSMOWidget::setDeviceValue(const int &deviceNum, const float &value) {
+    _labels.at(deviceNum + 8)->setText(convertFloatToString(value));
+}
+
+const char* TabSMOWidget::convertFloatToString(const float &value) const {
+    return std::to_string(value).c_str();
+}
+
--- src/ui/tabsmowidget.h
+++ src/ui/tabsmowidget.h
@@ -3,13 +3,24 @@
 #include <QWidget>
 #include <QLabel>
 #include <iostream>
+#include <vector>
+#include <string>
 
+//FIXME! need rename
 class TabSMOWidget : public QWidget {
 public:
     TabSMOWidget();
     void initSMO();
 
-    QLabel* label;
+    void setSourceValue(const int &sourceNum,     const float &value);
+    void setBufferValue(const int &bufferSlotNum, const float &value);
+    void setDeviceValue(const int &deviceNum,     const float &value);
+
+private:
+    const char *convertFloatToString(const float &value) const;
+private:
+    // тут будут храниться все устройства(буфер, источники и приборы)
+    std::vector<QLabel*> _labels;
 };
 
 #endif // TABSMOWIDGET_H
