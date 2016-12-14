7a22a5d72c1a64da76afa973eee856b31761f42c
rch_9 <enlofpoliteh121@gmail.com>
2016-12-14 21:38:57 +0300

    add modelcurrentstate

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 //#include "mainwindow.h"
 #include "resulttable.h"
-//#include "modelcurrentstate.h"
+#include "modelcurrentstate.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ResultTable w;
+    ModelCurrentState w;
     w.show();
 
     return a.exec();
